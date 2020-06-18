80279d326ac9f63e74b76e33b4548296eef9637e
Bhuwan Upadhyay <bhuwan.upadhyay@clusus.com>
2020-06-18 21:35:37 +0545

    chore: build fixes

--- gateway/src/main/java/io/github/bhuwanupadhyay/rtms/gateway/SecurityConfig.java
+++ gateway/src/main/java/io/github/bhuwanupadhyay/rtms/gateway/SecurityConfig.java
@@ -1,35 +1,35 @@
-package io.github.bhuwanupadhyay.rtms.gateway;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.oauth2.client.oidc.web.server.logout.OidcClientInitiatedServerLogoutSuccessHandler;
-import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
-import org.springframework.security.web.server.SecurityWebFilterChain;
-
-@Configuration
-@Profile({"prod"})
-public class SecurityConfig {
-
-  @Bean
-  public SecurityWebFilterChain springSecurityFilterChain(
-      ServerHttpSecurity http, ReactiveClientRegistrationRepository clientRegistrationRepository) {
-    // Authenticate through configured OpenID Provider
-    http.httpBasic().disable().formLogin().disable().oauth2Login();
-    // Also logout at the OpenID Connect provider
-    http.logout(
-        logout ->
-            logout.logoutSuccessHandler(
-                new OidcClientInitiatedServerLogoutSuccessHandler(clientRegistrationRepository)));
-    // Require authentication for all requests
-    http.authorizeExchange()
-        .pathMatchers("/actuator/health")
-        .permitAll()
-        .anyExchange()
-        .authenticated();
-    // Disable CSRF in the gateway to prevent conflicts with proxied service CSRF
-    http.csrf().disable();
-    return http.build();
-  }
-}
+//package io.github.bhuwanupadhyay.rtms.gateway;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.Profile;
+//import org.springframework.security.config.web.server.ServerHttpSecurity;
+//import org.springframework.security.oauth2.client.oidc.web.server.logout.OidcClientInitiatedServerLogoutSuccessHandler;
+//import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
+//import org.springframework.security.web.server.SecurityWebFilterChain;
+//
+//@Configuration
+//@Profile({"prod"})
+//public class SecurityConfig {
+//
+//  @Bean
+//  public SecurityWebFilterChain springSecurityFilterChain(
+//      ServerHttpSecurity http, ReactiveClientRegistrationRepository clientRegistrationRepository) {
+//    // Authenticate through configured OpenID Provider
+//    http.httpBasic().disable().formLogin().disable().oauth2Login();
+//    // Also logout at the OpenID Connect provider
+//    http.logout(
+//        logout ->
+//            logout.logoutSuccessHandler(
+//                new OidcClientInitiatedServerLogoutSuccessHandler(clientRegistrationRepository)));
+//    // Require authentication for all requests
+//    http.authorizeExchange()
+//        .pathMatchers("/actuator/health")
+//        .permitAll()
+//        .anyExchange()
+//        .authenticated();
+//    // Disable CSRF in the gateway to prevent conflicts with proxied service CSRF
+//    http.csrf().disable();
+//    return http.build();
+//  }
+//}
--- gateway/src/main/java/io/github/bhuwanupadhyay/rtms/gateway/SpringBoot.java
+++ gateway/src/main/java/io/github/bhuwanupadhyay/rtms/gateway/SpringBoot.java
@@ -2,10 +2,8 @@ package io.github.bhuwanupadhyay.rtms.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class SpringBoot {
 
   public static void main(String[] args) {
