b69b047aea612933c79d5c09963b270b98500789
Bhuwan Upadhyay <bhuwan.upadhyay@clusus.com>
2020-06-30 22:30:48 +0545

    chore(rtms): wip

--- inventory/inventory-service/src/main/java/io/github/bhuwanupadhyay/rtms/inventory/application/commandservices/InventoryCommandService.java
+++ inventory/inventory-service/src/main/java/io/github/bhuwanupadhyay/rtms/inventory/application/commandservices/InventoryCommandService.java
@@ -4,17 +4,20 @@ import io.github.bhuwanupadhyay.rtms.command.CommandService;
 import io.github.bhuwanupadhyay.rtms.command.WorkflowCommand;
 import io.github.bhuwanupadhyay.rtms.inventory.domain.commands.InventoryCreateCommand;
 import io.github.bhuwanupadhyay.rtms.inventory.domain.model.aggregates.Inventory;
-import io.github.bhuwanupadhyay.rtms.inventory.domain.model.factories.InventoryFactory;
 import io.github.bhuwanupadhyay.rtms.inventory.domain.model.valueobjects.InventoryId;
 import io.github.bhuwanupadhyay.rtms.inventory.infrastructure.repositories.jpa.InventoryDomainRepository;
 import io.github.bhuwanupadhyay.rtms.rules.Result;
 import io.github.bhuwanupadhyay.rtms.rules.SyntaxRules;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.hibernate.validator.HibernateValidator;
+import org.hibernate.validator.messageinterpolation.ResourceBundleMessageInterpolator;
+import org.hibernate.validator.resourceloading.PlatformResourceBundleLocator;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import static io.github.bhuwanupadhyay.rtms.inventory.domain.model.factories.InventoryFactory.VALIDATOR;
+import javax.validation.Validation;
+import javax.validation.Validator;
 
 @Service
 @RequiredArgsConstructor
@@ -22,12 +25,24 @@ import static io.github.bhuwanupadhyay.rtms.inventory.domain.model.factories.Inv
 public class InventoryCommandService
     implements CommandService<InventoryCreateCommand, InventoryId> {
 
+  private static final Validator VALIDATOR = Validation.byProvider(HibernateValidator.class)
+      .configure()
+      .messageInterpolator(new ResourceBundleMessageInterpolator(new PlatformResourceBundleLocator("ValidationMessages")))
+      .buildValidatorFactory().getValidator();
+
   private final InventoryDomainRepository repository;
 
   @Override
   @Transactional
   public Result<InventoryId> create(InventoryCreateCommand command) {
-    return savedResult(InventoryFactory.createNew(this.repository::nextId, command));
+    Result<InventoryCreateCommand> syntax = new SyntaxRules<InventoryCreateCommand>(VALIDATOR).apply(command);
+
+    Result<Inventory> result = syntax
+        .ok()
+        .map(c -> Result.onExecute(() -> new Inventory(this.repository.nextId()).execute(c)))
+        .orElseGet(() -> Result.<Inventory>builder().problems(syntax.getProblems()).build());
+
+    return savedResult(result);
   }
 
   @Override
@@ -37,7 +52,7 @@ public class InventoryCommandService
 
     Result<Inventory> result = syntax
         .ok()
-        .map(c -> Result.onExecute(() -> runWorkflow(command)))
+        .map(c -> Result.onExecute(() -> runWorkflow(c)))
         .orElseGet(() -> Result.<Inventory>builder().problems(syntax.getProblems()).build());
 
     return savedResult(result);
