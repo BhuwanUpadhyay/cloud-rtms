5392051e706769a6b0acf9f3e75b5a1e78ec6fca
Bhuwan Upadhyay <bhuwan.upadhyay@clusus.com>
2020-07-03 07:25:07 +0545

    chore(rtms): wip

--- workflow-engine/src/main/java/io/github/bhuwanupadhyay/rtms/workflow/WorkflowEngineApplication.java
+++ workflow-engine/src/main/java/io/github/bhuwanupadhyay/rtms/workflow/WorkflowEngineApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBoot {
+public class WorkflowEngineApplication {
   public static void main(String[] args) {
-    SpringApplication.run(SpringBoot.class, args);
+    SpringApplication.run(WorkflowEngineApplication.class, args);
   }
 }
